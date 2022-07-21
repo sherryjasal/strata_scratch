81e95324c8756ccada4b386e77aa629e4cf79d3c
RKaur <32834083+sherryjasal@users.noreply.github.com>
2022-07-21 09:00:57 +0530

    Create avg_salaries.py

---
+++ avg_salaries.py
@@ -0,0 +1,14 @@
+## Compare each employee's salary with the average salary of the corresponding department.
+## Output the department, first name, and salary of employees along with the average salary of that department.
+
+
+
+import pandas as pd
+
+# Start writing code
+#employee.head()
+df = employee.groupby(['department'],as_index=False).agg({'salary':'mean'}).rename(columns={'salary':'avg_salary'})
+pd.merge(employee[['department','first_name','salary']], df, on='department', how='left')
+
+## link https://platform.stratascratch.com/coding/9917-average-salaries?code_type=2
+## lessons_learnt = [[]] while merging for columns, secondly treat the new counted field as a new table and merge with old one
