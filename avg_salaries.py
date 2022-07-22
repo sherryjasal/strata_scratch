ac9f1c61e093b96608e833cd8e801d897ccd1d45
RKaur <32834083+sherryjasal@users.noreply.github.com>
2022-07-22 09:06:03 +0530

    Update avg_salaries.py

--- avg_salaries.py
+++ avg_salaries.py
@@ -11,4 +11,10 @@ df = employee.groupby(['department'],as_index=False).agg({'salary':'mean'}).rena
 pd.merge(employee[['department','first_name','salary']], df, on='department', how='left')
 
 ## link https://platform.stratascratch.com/coding/9917-average-salaries?code_type=2
+
+## Approach:
+## Create a new column that contains the average salary per group by using mean() within the transform() function to recombine the mean values to the grouped values
+## Use [ [ column_name/s] ] to return a specified column of the dataframe
+
+
 ## lessons_learnt = [[]] while merging for columns, secondly treat the new counted field as a new table and merge with old one
